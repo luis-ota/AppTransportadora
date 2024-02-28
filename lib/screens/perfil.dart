03dc923d98e53e0b4dae8362543806d0d657072f
Luis <luotasss@gmail.com>
2024-02-28 17:20:19 -0300

    arruando erros

--- lib/screens/perfil.dart
+++ lib/screens/perfil.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class PerfilPage extends StatefulWidget {
-  const PerfilPage({super.key});
+  const PerfilPage({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _PerfilPageState();
@@ -20,8 +20,7 @@ class _PerfilPageState extends State<PerfilPage> {
   bool _editMode = false;
   bool _editSenhaMode = false;
   bool _editouNome = false;
-
-  // bool _editouEmail = false;
+  bool _editouEmail = false;
 
   @override
   void initState() {
@@ -31,7 +30,7 @@ class _PerfilPageState extends State<PerfilPage> {
     _senhaController.text = '';
     _editMode = false;
     _editouNome = false;
-    // _editouEmail = false;
+    _editouEmail = false;
   }
 
   @override
@@ -44,29 +43,31 @@ class _PerfilPageState extends State<PerfilPage> {
       ),
       body: SingleChildScrollView(
         child: SizedBox(
-          height: 500,
+          height: MediaQuery.of(context).size.height - 200,
+          width: MediaQuery.of(context).size.width,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
+              Column(
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    child: user!.photoURL == null
+                        ? const Icon(Icons.person, size: 50)
+                        : null,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        CircleAvatar(
-                          radius: 50,
-                          child: user!.photoURL == null
-                              ? const Icon(Icons.person, size: 50)
-                              : null,
-                        ),
-                        const SizedBox(height: 20),
                         SizedBox(
-                          width: 350,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          width: 170,
+                          child: Column(
                             children: [
                               _editMode
                                   ? _buildEditableField(
@@ -74,38 +75,42 @@ class _PerfilPageState extends State<PerfilPage> {
                                   : _buildReadOnlyField(!_editouNome
                                       ? "Nome: ${user!.displayName}"
                                       : "Nome: ${_nameController.text}"),
-                              const SizedBox(
-                                width: 20,
-                              ),
-                              IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _editMode = !_editMode;
-                                    if (!_editMode) {
-                                      _saveChanges();
-                                    }
-                                  });
-                                },
-                                icon: Column(
-                                  children: [
-                                    const Icon(Icons.edit),
-                                    Text(_editMode ? 'Salvar' : 'Editar'),
-                                  ],
-                                ),
-                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _editMode = !_editMode;
+                              if (!_editMode) {
+                                _saveChanges();
+                              }
+                            });
+                          },
+                          icon: Column(
+                            children: [
+                              const Icon(Icons.edit),
+                              Text(_editMode ? 'Salvar' : 'Editar'),
                             ],
                           ),
                         ),
                       ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               Column(
                 children: [
                   Visibility(
                     visible: _editSenhaMode,
-                    child: _buildEditableField(_senhaController, 'Nova Senha'),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 30, right: 30),
+                      child:
+                          _buildEditableField(_senhaController, 'Nova Senha'),
+                    ),
                   ),
                   const SizedBox(height: 20),
                   ElevatedButton(
@@ -183,13 +188,10 @@ class _PerfilPageState extends State<PerfilPage> {
 
   Widget _buildEditableField(
       TextEditingController controller, String hintText) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          hintText: hintText,
-        ),
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: hintText,
       ),
     );
   }
@@ -202,9 +204,7 @@ class _PerfilPageState extends State<PerfilPage> {
   }
 
   Future<void> _saveChanges() async {
-    if (_nameController.text.isNotEmpty) {
-      user!.updateDisplayName(_nameController.text);
-    }
+    user!.updateDisplayName(_nameController.text);
     // user!.verifyBeforeUpdateEmail('${_emailController.text}@apprubinho.com');
     if (_senhaController.text.isNotEmpty) {
       try {
@@ -222,7 +222,7 @@ class _PerfilPageState extends State<PerfilPage> {
     }
     if (_emailController.text.isNotEmpty) {
       setState(() {
-        // _editouEmail = true;
+        _editouEmail = true;
       });
     }
   }
@@ -232,12 +232,8 @@ class _PerfilPageState extends State<PerfilPage> {
       ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Senha atualizada com sucesso')));
     } else {
-      _senhaController.text = '';
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-            duration: Duration(seconds: 8),
-            content: Text(
-                'Erro ao atualizar a senha: a senha deve conter pelo menos 6 caracteres')),
+        SnackBar(content: Text('Erro ao atualizar a senha: $erro')),
       );
     }
   }
